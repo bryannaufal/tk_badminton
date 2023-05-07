dbda46a51313c32da3127242342839a8d1d019e3
Rahmat Bryan Naufal <rahmat.bryan@ui.ac.id>
2023-05-07 18:12:53 +0700

    fix nav pelatih

--- babadu/forms.py
+++ babadu/forms.py
@@ -6,11 +6,18 @@ from django.contrib.auth.models import User
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
+	perans = (
+	('Atlet', 'Atlet'),
+	('Pelatih', 'Pelatih'),
+	('Umpire', 'Umpire'),
+	)
 	email = forms.EmailField(required=True)
+	peran = forms.ChoiceField(choices=perans)
+
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "email", "password1", "password2", "peran")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
