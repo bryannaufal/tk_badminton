25a1b50e5e2bb9f2ccc78a42e582e4b55be17e8c
Astrid Swardhani Putri <astrid.swardhani@ui.ac.id>
2023-05-23 20:54:24 +0700

    integrating django and railway

--- project_django/settings.py
+++ project_django/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'project_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TZIrS5uMrMDOvtEk1ySG',
+        'HOST': 'containers-us-west-60.railway.app',
+        'PORT': '6602',
     }
 }
 
