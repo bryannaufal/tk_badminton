6ac8e6e45b3f204538b0ef4c48d10c1f8b2e3c31
Nadira Maysa Dyandra <nadira.maysa@gmail.com>
2023-05-25 18:21:39 +0700

    Tambah dashboard atlet

--- atlet/urls.py
+++ atlet/urls.py
@@ -11,11 +11,11 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('dashboard_atlet/', dashboard_atlet, name='dashboard_atlet'),
     path('atlet_ujian_kualifikasi_list/', atlet_ujian_kualifikasi_list, name='atlet_ujian_kualifikasi_list'),
     path('atlet_ujian_kualifikasi_riwayat/', atlet_ujian_kualifikasi_riwayat, name='atlet_ujian_kualifikasi_riwayat'),
     path('atlet_ujian_kualifikasi_soal/', atlet_ujian_kualifikasi_soal, name='atlet_ujian_kualifikasi_soal'),
     path('atlet_daftar_stadium/', atlet_daftar_stadium, name='atlet_daftar_stadium'),
     path('atlet_daftar_stadium/<str:stadium>', atlet_daftar_event, name='atlet_daftar_event'),
-    path('atlet_daftar_stadium/<str:stadium>/<str:event>', atlet_daftar_partai, name='atlet_daftar_partai')
+    path('atlet_daftar_stadium/<str:stadium>/<str:event>', atlet_daftar_partai, name='atlet_daftar_partai'),
+    path('dashboard_atlet/', dashboard_atlet, name='dashboard_atlet')
 ]
