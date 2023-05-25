f6b1e45af7e255580fd00eb4456edcc49cf1c199
Rahmat Bryan Naufal <rahmat.bryan@ui.ac.id>
2023-05-25 08:52:35 +0700

    login done

--- babadu/views.py
+++ babadu/views.py
@@ -45,7 +45,6 @@ def register_atlet(request):
 
 
         with connection.cursor() as cursor:
-            response = ""
             cursor.execute("""
                             SELECT *
                             FROM member
@@ -70,11 +69,31 @@ def register_atlet(request):
     
 def register_pelatih(request):
     if request.method == 'POST':
+        id = uuid1()
         nama = request.POST.get('nama')
         email = request.POST.get('email')
         negara = request.POST.get('negara')
         kategori = request.POST.get('kategori')
         tanggal_mulai = request.POST.get('tanggal_mulai')
+    
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                            SELECT *
+                            FROM member
+                            WHERE email ='{email}';
+                            """)
+
+            is_email_exist = cursor.fetchall()
+            if is_email_exist:
+                messages.info(request, 'Email sudah pernah terdaftar!')
+            else:
+                cursor.execute(f"""INSERT INTO member VALUES ('{id}', '{nama}', '{email}')""")
+                connection.commit()
+                cursor.execute(f"INSERT INTO pelatih VALUES ('{id}', '{tanggal_mulai}')")
+                connection.commit()
+                messages.success(request, 'Akun telah berhasil dibuat!')
+                return redirect('/pelatih')
+            print(is_email_exist)
 
     context = {}
     return render(request, 'register_pelatih.html', context)
@@ -102,15 +121,64 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login(request):
+    with connection.cursor() as cursor:
+            cursor.execute("""
+                            SELECT member.nama, member.email
+                            FROM member, atlet
+                            WHERE atlet.id = member.id;
+                            """)
+
+            atlet_data = cursor.fetchall()
+            cursor.execute("""
+                            SELECT member.nama, member.email
+                            FROM member, pelatih
+                            WHERE pelatih.id = member.id;
+                            """)
+
+            pelatih_data = cursor.fetchall()
+            cursor.execute("""
+                            SELECT member.nama, member.email
+                            FROM member, umpire
+                            WHERE umpire.id = member.id;
+                            """)
+
+            umpire_data = cursor.fetchall()
+
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('babadu:show_wishlist')
+        nama = request.POST.get('nama')
+        email = request.POST.get('email')
+
+        is_valid = False
+        role = ""
+        for element in atlet_data:
+            print(element)
+            print(nama)
+            print(email)
+            if nama == element[0] and email == element[1]:
+                is_valid = True
+                role = 'atlet'
+                request.session['role'] = role
+                break
+        for element in pelatih_data:
+            if nama == element[0] and email == element[1]:
+                is_valid = True
+                role = 'pelatih'
+                request.session['role'] = role
+                break
+        for element in umpire_data:
+            if nama == element[0] and email == element[1]:
+                is_valid = True
+                role = 'umpire'
+                request.session['role'] = role
+                break
+
+        if is_valid:
+            request.session['nama'] = nama
+            request.session['email'] = email
+            messages.success(request, f'Anda telah berhasil login sebagai {role} :)')
+            return redirect(f'/{role}')
         else:
-            messages.info(request, 'Username atau Password salah!')
+            messages.info(request, f'Data yang anda masukkan tidak valid :(')
     context = {}
     return render(request, 'login.html', context)
 
